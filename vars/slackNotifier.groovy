f223c1ec5fc14637dfd5ee1ecb7c75709a1cc616
Miguel <mivimur@hotmail.com>
2019-01-13 00:41:33 +0100

    fix(Slack) : fix executable

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,3 +1,5 @@
+#!/usr/bin/env groovy
+
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
     slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
