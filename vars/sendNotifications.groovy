4e7d99704311445487a336d303c6e02530cf21d6
Miguel <mivimur@hotmail.com>
2019-01-13 10:20:40 +0100

    refactor(Slack) : refactor sendNotifications.groovy

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -76,11 +76,11 @@ private JSONObject createCommitMessage() {
     //def commit = sh(returnStdout: true, script: 'git rev-parse HEAD')
     def message = sh(returnStdout: true, script: 'git log -1 --pretty=%B').trim()
 
-    commitMess = "Commit Message < ${env.RUN_CHANGES_DISPLAY_URL} |  changes )>"
+    commitMess = "Commit Message < ${env.RUN_CHANGES_DISPLAY_URL} |  show changes )>"
 
     final JSONObject commitMessage = new JSONObject()
     commitMessage.put('title', commitMess.toString())
-    commitMessage.put('value', message.toString())
+    commitMessage.put('value', message.toString() + "\n\n" + commitMess)
     commitMessage.put('short', false)
     commitMessage
 }
