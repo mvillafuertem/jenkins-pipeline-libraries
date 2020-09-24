c7fbd94bfd43ee35d147b547be0537667a84a7d6
Miguel Villafuerte <mivimur@hotmail.com>
2020-09-24 17:29:11 +0200

    doc(project) : add information about grape

--- src/io/github/mvillafuertem/aws/AwsWorkflow.scala
+++ src/io/github/mvillafuertem/aws/AwsWorkflow.scala
@@ -8,7 +8,8 @@ import scala.jdk.CollectionConverters._
 // Because it is not a groovy class
 // You get an org.jenkinsci.plugins.workflow.cps.CpsCompilationErrorsException
 // It is only to toy with scala-test
-// You need create a jenkins plugin to use scala in jenkins pipeline
+// You need create a jar or jenkins plugin to use scala in jenkins pipeline
+// If is a jar use http://docs.groovy-lang.org/latest/html/documentation/grape.html#_quick_start
 final class AwsWorkflow(script: Script) extends Serializable {
 
   def call(): Unit = {
